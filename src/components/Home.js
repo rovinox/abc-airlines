269b970cfac2ac3f401ff966d7e7d39c93b1ea55
neaj mahmud <neajmahmud230@gmail.com>
2020-01-31 00:15:59 -0500

    working on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import TextLoop from "react-text-loop";
 
 export default function Home() {
     return (
         <div className="home">
-        home
+            <div className="loop">
+
+            <h2>
+                <TextLoop springConfig={{ stiffness: 180, damping: 8 }}>
+                    <span>First item1</span>
+                    <span>First item2</span>
+                    <span>First item3</span>
+                    <span>First item4</span>
+                    
+                </TextLoop>
+                
+            </h2>
+            </div>
         </div>
     )
 }
